7303f6214c80a82ae6da76175a766fca289e6e83
Kleinanzeige386 <95414855+Kleinanzeige386@users.noreply.github.com>
2022-02-10 10:24:23 +0000

    ignore

--- user_system.c
+++ user_system.c
@@ -7,3 +7,41 @@
 // AVAILABLE HERE: Function print_user(id, name, email) to print a single user
 // Make sure to write empty functions for ALL functions in the header (also optional ones),
 // otherwise the program will not compile
+
+
+
+
+typedef int user_id;
+
+typedef struct Liste{
+    user_id id;
+    char *name;
+    char *eMail;
+    struct Liste *next;
+}Liste;
+
+Liste* goToEnd(Liste anfang){
+
+}
+
+void users_initialize_table(int capacity){
+    
+    if (capacity>0){
+
+        Liste start={.id=0, .next=NULL};
+    
+        for (int i = 1; i < capacity; i++)
+        {
+        
+        }
+    }
+
+
+}
+
+
+user_id users_add(char *name, char *email);
+
+void users_printall();
+
+char *users_get_name(user_id id);
